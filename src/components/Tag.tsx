18648788af79c7f7c0b2855f8c1657162a871453
Zem860 <zemmyhammy@gmail.com>
2024-10-21 17:04:03 +0800

    新增tagwords和verticalwords做區分

--- src/components/Tag.tsx
+++ src/components/Tag.tsx
@@ -1,7 +1,7 @@
 const Tag = () => {
     return ( 
         <>
-            <div className=" ml-3 p-5 flex justify-center items-center px-1 bg-[#3F5D45] vertical-words text-white">
+            <div className=" ml-3 p-5 flex justify-center items-center px-1 bg-[#3F5D45] text-5xl md:text-2xl tag-words text-white">
                 <p>本日精選</p>
             </div>
         
