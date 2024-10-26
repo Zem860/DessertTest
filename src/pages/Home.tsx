e896b40b34025177529d00d4a8b53caa575d29da
Zem860 <zemmyhammy@gmail.com>
2024-10-26 17:39:01 +0800

    fix:修正主頁面 key在map中回圈放置錯誤的問題

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -10,15 +10,24 @@ const Home = () => {
       <MainContentCard reverse={true} />
       <MainContentCard reverse={false} />
       <div className="flex justify-center items-center">
-        <Slogan words1={'想吃甜點——'} words2={'是不需要理由的'} size="text-5xl" singleSlogan alwaysVertical={false}/>
+        <Slogan
+          words1={'想吃甜點——'}
+          words2={'是不需要理由的'}
+          size="text-5xl"
+          singleSlogan
+          alwaysVertical={false}
+        />
       </div>
       <div className=" p-6 md:p-0 my-24 mx-auto text-center justify-items-center grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
-      {imagesLinksHompage.map((image, index) => (
-        index===0?<Card key={index} bgBackground={image} />:
-        <div className=" hidden md:block">
-          <Card key={index} bgBackground={image} />
-        </div>
-      ))}
+        {imagesLinksHompage.map((image, index) =>
+          index === 0 ? (
+            <Card key={index} bgBackground={image} />
+          ) : (
+            <div className=" hidden md:block " key={index}>
+              <Card bgBackground={image} />
+            </div>
+          ),
+        )}
       </div>
     </>
   );
